85054e18d7d5c8a8a095a7decd9695d4213ff5eb
dalalbn <148500813+dalalbn@users.noreply.github.com>
2025-04-28 23:39:42 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,5 +1,5 @@
 -- Create LabDatabase Database
-
+CREATE DATABASE LabDatabase
 GO
 
 -- Use LabDatabase Database
@@ -7,9 +7,18 @@ USE LabDatabase;
 GO 
 
 -- Create Laboratoire Table
-
+CREATE TABLE Laboratoire ( 
+  CodeLab VARCHAR(4) PRIMARY KEY , 
+  NomLab VARCHAR (100) , 
+  Datcreation DATE , 
+  DatFin DATE ,
+Siteweb NVARCHAR (255), ) ; 
 
 -- Create Chercheur Table 
-
+CREATE TABLE Chercheur ( 
+  NumCh int PRIMARY kEY , 
+  NomCh VARCHAR(30) , 
+  CodeLab VARCHAR(40) , 
+  FOREIGN KEY (CodeLab ) REFERENCES Laboratoire(CodeLab) );
 
 GO
